d6994ca5d4656257e7071d3ffae0ae8f75ad0ffc
Оleksandr Bozhko <mamaliuk.alina@gmail.com>
2025-02-25 23:00:40 +0200

    hw-10

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,47 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+document.querySelector(".form").addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const delay = Number(form.elements.delay.value);
+    const promiseState = form.elements.state.value;
+
+    // Checking for an incorrect delay value
+    if (delay <= 0) {
+        iziToast.warning({
+            title: "⚠ Warning",
+            message: "Delay must be greater than 0ms",
+            position: "topRight",
+            timeout: 3000,
+        });
+        return;
+    }
+
+    createPromise(delay, promiseState)
+        .then((delay) => {
+            iziToast.success({
+                title: "✅ OK",
+                message: `Fulfilled promise in ${delay}ms`,
+                position: "topRight",
+                timeout: 5000,
+            });
+        })
+        .catch((delay) => {
+            iziToast.error({
+                title: "❌ Error",
+                message: `Rejected promise in ${delay}ms`,
+                position: "topRight",
+                timeout: 5000,
+            });
+        });
+
+    form.reset();
+});
+
+function createPromise(delay, promiseState) {
+    return new Promise((resolve, reject) =>
+        setTimeout(() => (promiseState === "fulfilled" ? resolve(delay) : reject(delay)), delay)
+    );
+}
